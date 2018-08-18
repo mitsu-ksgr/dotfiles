58b3e47c8f84b5ab95726b3f9f89e5baae31a263
mitsu-ksgr <k.sgr.mitsu.g@gmail.com>
2018-08-18 18:16:34 +0900

    improve

--- tools/gpdify.sh
+++ tools/gpdify.sh
@@ -3,15 +3,14 @@
 #
 # gpd pocket setup
 #
-
 if [ "`whoami`" != "root" ]; then
     echo "Require root privilage."
     echo "run to \`sudo ./apply.sh\`"
     exit 1
 fi
 
-my_path="$(cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")"; pwd)"
-fname="$(basename ${0})"
+# my_path="$(cd "$(dirname "${BASH_SOURCE:-${(%):-%N}}")"; pwd)"
+# fname="$(basename ${0})"
 
 rfpath=""
 if [[ -L "${0}" ]]; then
@@ -34,6 +33,6 @@ wdev_name="wlp0s20u4"
 sudo ip link set "${wdev_name}" up
 sudo wifi-menu "${dev_name}"
 
-exit 0
 
+exit 0
 
