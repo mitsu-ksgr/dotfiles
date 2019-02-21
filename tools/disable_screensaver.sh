48cd6f6c8f30b8b762e0f036df809179c12bb458
mitsu-ksgr <k.sgr.mitsu.g@gmail.com>
2019-02-21 12:51:57 +0900

    tools: add disable screensaver script.

---
+++ tools/disable_screensaver.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#
+# Disable screensaver
+# cf. https://superuser.com/questions/644804/disable-screensaver-screen-blank-via-command-line/644829#644829
+#
+
+xset s off
+xset s noblank
+xset -dpms
+
